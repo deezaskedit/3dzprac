ae7626efb1e0f2cbd9b5b1e43f52d533ae14fa4d
Artem <aneday05@mail.ru>
2022-08-31 22:05:52 +0300

    1zd completed

--- zd19/Program.cs
+++ zd19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Palindrome(int number)
+{
+    int inverse = 0;
+    int newnumber = number;
+    while(number > 0)
+    {
+        int rem = number % 10;
+        inverse = inverse * 10 + rem;
+        number = number/10;
+    }
+    if(newnumber == inverse) Console.WriteLine($"{inverse} - Число является палиндромом");
+    else Console.WriteLine($"{inverse} - Число не является палиндромом");
+}
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrome(num);
