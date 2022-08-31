19b1d6190eb49098bebc49443c0b9e415d908579
Artem <aneday05@mail.ru>
2022-08-31 22:09:17 +0300

    2zd completed

--- zd21/Program.cs
+++ zd21/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double FindDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+}
+Console.WriteLine("Enter the first dot (x, y, z): ");
+double xA = Convert.ToDouble(Console.ReadLine());
+double yA = Convert.ToDouble(Console.ReadLine());
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the second dot (x, y, z): ");
+double xB = Convert.ToDouble(Console.ReadLine());
+double yB = Convert.ToDouble(Console.ReadLine());
+double zB = Convert.ToDouble(Console.ReadLine());
+double dist = FindDistance(xA, yA, zA, xB, yB, zB);
+Console.WriteLine($"Distance is {dist}");
