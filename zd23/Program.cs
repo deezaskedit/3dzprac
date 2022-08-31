105dc81c8aee99096b741ffb6851aa20327b3dfe
Artem <aneday05@mail.ru>
2022-08-31 22:17:15 +0300

    3zd completed

--- zd23/Program.cs
+++ zd23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+void Cube(int number)
+{
+    int current = 1;
+    while(current <= number)
+    {
+        Console.Write(Math.Pow(current, 3) + " ");
+        current ++;
+    }
+}
+Console.Write("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Cube(num);
